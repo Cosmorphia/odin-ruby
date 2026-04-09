db7a050afd7eb586485265ab43736b11aeea00d8
Cosmorphia <179248894+Cosmorphia@users.noreply.github.com>
2026-04-08 20:32:19 -0400

    Add alternative bubble sort solution

--- bubble-sort/script.rb
+++ bubble-sort/script.rb
@@ -22,6 +22,21 @@ def bubble_sort(array)
   array
 end
 
+# not quite the true bubble sort algorithm, but still wanted to try it
+# highest value still "bubbles" but more than two values can be compared at once
+# the grouped numbers can be reversed as they will only be grouped in a strictly descending order
+# breaks loop if there are no grouped numbers
+
+def bubble_sort2(array)
+  loop do
+    array = array.chunk_while{ |i, k| i > k }.to_a
+    break array.flatten if array.all?{ |e| e.length == 1 }
+    array = array.map(&:reverse).flatten
+  end
+end
+
 array = [4, 3, 78, 2, 0, 2]
+array2 = [40, 10, 7, 11, 1, 15]
 
 p bubble_sort(array)
+p bubble_sort2(array2)
